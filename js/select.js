c9bd7ecdf85e7a341dc82b9dd21dd554fbe627df
Sion <sionn908@gmail.com>
2024-03-04 02:12:06 +0900

    ページ選択を追加

---
+++ js/select.js
@@ -0,0 +1,7 @@
+document.getElementById('gotoPortfolio').addEventListener('click', function() {
+    window.location.href = './ar-main.html'; // ここにポートフォリオページのURLを設定
+});
+
+document.getElementById('experienceAR').addEventListener('click', function() {
+    window.location.href = './'; // ここにAR体験ページのURLを設定
+});
