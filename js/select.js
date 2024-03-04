38ef1dda0b74ab7594a0488134552102c6dccc64
Sion <sionn908@gmail.com>
2024-03-04 15:28:39 +0900

    ルーティングミス

--- js/select.js
+++ js/select.js
@@ -1,7 +1,7 @@
 document.getElementById('gotoPortfolio').addEventListener('click', function() {
-    window.location.href = './ar-main.html'; // ここにポートフォリオページのURLを設定
+    window.location.href = './'; // ここにポートフォリオページのURLを設定
 });
 
 document.getElementById('experienceAR').addEventListener('click', function() {
-    window.location.href = './'; // ここにAR体験ページのURLを設定
+    window.location.href = './ar-main.html'; // ここにAR体験ページのURLを設定
 });
